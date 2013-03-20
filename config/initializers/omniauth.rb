bf3654348bc9cc895b30fa0db780d5965f3bb937
cipher-technologies <sumit.munot@cipher-tech.com>
2013-03-20 16:16:57 +0530

    change url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
-OmniAuth.config.full_host = "http://localhost:3000/"
+OmniAuth.config.full_host = "http://facebook-invitition-staging.herokuapp.com/"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,  352901268149640, :a84a54acd45baa3fb85143393bb90b42, :iframe => true, 
